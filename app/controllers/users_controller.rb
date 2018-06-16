7d8d54aabf2f83079a32bed3d19c1c1e91004c55
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-16 12:53:35 +0900

    ユーザー情報更新における『authenticate_user!』の設定＆コメント追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit]
 
+  before_action :authenticate_user!, only: [:edit, :update]
+  # ログイン済みのユーザーのみ、only以下のアクション実行可能。
+  # 未ログイン時の場合は、サインインページへリダイレクトされる。
+
   def show
   end
 
@@ -10,6 +14,9 @@ class UsersController < ApplicationController
   def update
     # 『current_user』に対してupdateメソッドを使うと、1行で書ける。
     current_user.update(user_params)
+    # 『before_action』に『update』を設定している場合、下記の記述でも良い。
+    # @user = update(user_params)
+
 
     redirect_to root_path
   end
