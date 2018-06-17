2872b883f7f8bcf6af2f2dd8c3b4e4e864e12d73
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-18 00:22:35 +0900

    アソシエーションの設定

--- app/models/captured_image.rb
+++ app/models/captured_image.rb
@@ -1,4 +1,6 @@
 class CapturedImage < ActiveRecord::Base
+  belongs_to :prototype
+
   validates :content,
             :status,
             presence: true
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,10 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
+  has_many   :captured_images, dependent: :destroy
+
+  # 『protoytpe』にネストされた『captured_image』に対して投稿できるようにする。
+  # ネストされているモデルを記載。キーではない。
+  accepts_nested_attributes_for :captured_images
 
   validates :title,
             :catch_copy,
