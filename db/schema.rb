03f8150f74031dea9b164d90ce9a179374832019
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-18 03:10:07 +0900

    DBの修正（サブ画像が存在しない時に保存を許す為）

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@
 ActiveRecord::Schema.define(version: 20180617143000) do
 
   create_table "captured_images", force: :cascade do |t|
-    t.string   "content",      limit: 255, null: false
-    t.integer  "status",       limit: 4,   null: false
+    t.string   "content",      limit: 255
+    t.integer  "status",       limit: 4
     t.integer  "prototype_id", limit: 4,   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
