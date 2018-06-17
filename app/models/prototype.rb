d214e3b5335650d5b233bfdf3e03924f5ee0a050
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-18 02:28:53 +0900

    メモ書き記載

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,6 +5,7 @@ class Prototype < ActiveRecord::Base
   # 『protoytpe』にネストされた『captured_image』に対して投稿できるようにする。
   # ネストされているモデルを記載。キーではない。
   accepts_nested_attributes_for :captured_images
+  # コントローラー側には『captured_images_attributes: [:content]』でパラメータ許可する
 
   validates :title,
             :catch_copy,
