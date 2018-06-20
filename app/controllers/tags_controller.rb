426ff870d85aa348c0ad6527572e8dd8592ca0f2
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-21 01:56:26 +0900

    tagsコントローラーのshowアクションの設定

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,5 +4,11 @@ class TagsController < ApplicationController
   end
 
   def show
+    @tag = Tag.find_by(name: params[:name])
+    @prototypes = @tag.prototypes.includes(:user).order("created_at ASC").page(params[:page]).per(5)
+
+    # 他のコントローラーのアクションへ飛ばしたい場合は下記の記述。
+    # 今回、飛んだ先で同フォルダ内の部分テンプレートを呼び出している為、使用しない。
+    # render template: "prototypes/index"
   end
 end
