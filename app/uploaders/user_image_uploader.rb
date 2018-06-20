76998f08b24949252fa6fb880e67b7683bc21cbe
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-21 02:54:34 +0900

    2つのuploaderファイルの環境違いによる画像の保存先の変更

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -4,7 +4,12 @@ class UserImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
+  # storage :file：上記追加に伴い、コメアウト
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
