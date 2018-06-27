ecb90546c045e48bc48ca626fd5c95cdc9947cc8
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-27 20:52:59 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
+  # root "prototypes#index", path: "popular"
+
+  # 『prototypes』以降に記述してしまうと、url打ち込み時に『/prototypes/:id』の方が先に来て、prototype#showへアクセスしてしまう。
+  resources :newests, path: "prototypes/newest", only: :index
 
   # 『root』でindexを設定している為、下記にindexは不要となる。
   resources :prototypes, except: :index do
     resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
   end
+
+  # 通常のルーティング記載
+  # resources :newests, only: :index
+  # リソースベースで呼び出すコントローラ＆アクションに対して、URLを変更する場合
+  # URLヘルパーの名前は変わらない。
+  # resources :newests, path: "prototypes/newest", only: :index
+  # 『controller:』の記述でも書くことが可能（URLヘルパーの名前は変わる）
+
+  # namespace :prototypes do
+  #   resources :newests, path: "newest", only: :index
+  # end
+
   # 『param: :name』を記載することで、『:id』部分の変更が可能。
   # 下記の方法以外に、『to_param』をモデルに定義する方法もある。
   resources :tags, param: :name, only: [:index, :show]
